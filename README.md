ed080f6526b611640cbc3eba384953a308911d55
Dina <counting1121@yahoo.com>
2018-12-14 19:42:55 -0500

    Final Giphy app

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Giftastic
+
+This is an app that allows you to see images of some of the best places in the world to go on vacation!
+
+Using a link to the Giphy site, the program assigns still and animated images to each places button.
+
+Click on the location button to see the images of that location.
+
+Click on the image to see the animated version and click again to go back to the still version.
+
+There is a text box where you can add your own favorite location.
+
+Enter your location and hit the add button. A button of your spot will be added to the other buttons.
+
+Click on your button to see images of your location!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,64 +2,100 @@
 $(document).ready(function () {
 
   // Setting up an array of 10 great vacation spots
-  var topics = ["Rome", "Cancun", "London", "Miami", "Orlando", "Jamaica", "San Francisco", "Hawaii", "Paris","NYC"];
+  var topics = ["Rome", "Cancun", "London", "Miami", "Orlando", "Jamaica", "San Francisco", "Hawaii", "Paris", "NYC"];
 
   // displayVacationSpot function re-renders the HTML to display the appropriate content
   function displayVacationSpot() {
 
+      //Setting up global variable to take in spot name
     var topic = $(this).attr("spot-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LROvVF2eoeCLQzzJ7fxLdyblEINc4dSo&q="+ topic +"&limit=10&offset=0&rating=G&lang=en";
-    
+
+    //Linking to Giphy with my API key and a variable for the topic, using parameters of 10 pictures and a rating of G
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LROvVF2eoeCLQzzJ7fxLdyblEINc4dSo&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+
+      //Empties any vacation spots that may be there
     $("#vacation-view").empty();
+
     // Creating an AJAX call for the specific vacation button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
+      //For loop to go through my 10 spots, linking a path to an image for them and setting up the button
       for (var j = 0; j < 10; j++) {
 
-      // Creating a div to hold the vacation spot
-      var topicDiv = $("<div class='vacation'>");
-        
-      // Storing the rating data
-      var ratingG = response.data[j].rating;
+        // Creating a div to hold the vacation spot
+        var topicDiv = $("<div class='vacation'>");
+        topicDiv.attr("style", "display: inline-block");
 
-      // Creating an element to have the rating displayed
-      var pOne = $("<p>").text("Rating: " + ratingG);
+        // Storing the rating data
+        var ratingG = response.data[j].rating;
 
-      // Displaying the rating
-      topicDiv.append(pOne);
-          
-      // Retrieving the URL for the image
-      var imgURL = response.data[j].images.fixed_height_downsampled.url;
-      
-      // Creating an element to hold the image
-      var image = $("<img>").attr("src", imgURL);
+        // Creating an element to have the rating displayed
+        var pOne = $("<p>").text("Rating: " + ratingG);
 
-      // Appending the image
-      topicDiv.append(image);
+        // Displaying the rating
+        topicDiv.append(pOne);
 
-      // Putting the pictures above the previous pictures
-      $("#vacation-view").prepend(topicDiv);
+        // Retrieving the URL for the images, a still one and an animated one
+        var imgStill = response.data[j].images.fixed_height_still.url;
+        var imgAnimate = response.data[j].images.fixed_height_downsampled.url;
+
+
+        // Creating an element to hold the image and assigning attributes to it
+        var image = $("<img>");
+        image.attr("src", imgStill);
+        image.attr("data-still", imgStill);
+        image.attr("data-state", "still");
+        image.attr( "data-animate", imgAnimate);
+        image.addClass("gif");
+        image.attr("style", "padding:10px");
+        image.on("click", changeImage)
+
+        // Appending the image
+        topicDiv.append(image);
+
+
+        // Putting the pictures above the previous pictures
+        $("#vacation-view").prepend(topicDiv);
       }
+
+
     });
 
   }
+
+  //Function to change picture between the still one and the animated one on a click of the picture
+  function changeImage() {
+
+    // Variable picks up the data-state attribute, still or animate
+    var state = $(this).attr("data-state");
+
+    //Checks what the data-state is and changes it to the opposite state
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+      
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  }
+
+
 
   // Function for displaying vacation spot data
   function renderButtons() {
 
     // Deleting the vacation spots prior to adding new vacation spots
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+        $("#buttons-view").empty();
 
     // Looping through the array of locations
     for (var i = 0; i < topics.length; i++) {
 
       // Then dynamicaly generating buttons for each vacation spot in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
+            var a = $("<button>");
       // Adding a class of vacation-btn to our button
       a.addClass("vacation-btn");
       // Adding a spot-attribute
@@ -71,8 +107,8 @@ $(document).ready(function () {
     }
   }
 
-  // This function handles events where a location button is clicked
-  $("#add-spot").on("click", function(event) {
+  // Function to take input from the text box and create a new button with a new location
+  $("#add-spot").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var topic = $("#vacation-input").val().trim();
@@ -82,7 +118,9 @@ $(document).ready(function () {
 
     // Calling renderButtons which handles the processing of our topics array
     renderButtons();
+
   });
+
 
   // Adding a click event listener to all elements with a class of "vacation-btn"
   $(document).on("click", ".vacation-btn", displayVacationSpot);
@@ -91,3 +129,4 @@ $(document).ready(function () {
   renderButtons();
 
 })
+
